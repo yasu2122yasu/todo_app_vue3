79596b9f3ab91e9a5b337938fbba6cec83cf2f97
yasu2122yasu <ya2su1shi2@icloud.com>
2023-02-25 11:07:22 +0900

    コメント

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+import MainToDo from './components/MainToDo.vue';
+</script>
+
+<template></template>
+<style>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: 100vh;
+  width: 370px;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+
+.main {
+  flex: 1;
+  width: 100%;
+}
+</style>
